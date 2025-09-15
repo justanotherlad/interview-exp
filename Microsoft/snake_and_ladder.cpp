cfd75baba4c8d378b27428cc8b65b1a5f2666235
Swastik Banerjee <swastik_banerjee07@yahoo.co.in>
2025-09-15 16:58:29 +0530

    Create snake_and_ladder.cpp

---
+++ Microsoft/snake_and_ladder.cpp
@@ -0,0 +1,52 @@
+class Game{
+
+	bool isOn(){
+		
+	}
+
+}
+
+
+
+class User{
+	int color;
+	int pos;
+
+	User(int cColor, int pPos)
+
+
+}
+
+class Board{
+
+	int pos;
+
+	bool isSnake(int pos){
+
+	}
+
+	bool isLadder(int pos){
+
+	}
+
+}
+
+class Snake:public Board{
+	int startpos;
+	int endpos;
+
+
+
+
+
+}
+
+class Lader:public Board{
+	int startpos;
+	int endpos;
+
+
+}
+
+
+
